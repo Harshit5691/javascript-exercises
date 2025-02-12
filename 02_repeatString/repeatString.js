71b1ac6de08b04c1bd655d7b512cf70fc8fdc059
Harshit5691 <harshit2.mitmpl2022@learner.manipal.edu>
2025-02-12 20:07:42 +0530

    q2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word,times) {
+    let str = "";
+    for(let i=0;i<times;i++){
+        if(times<0){
+            return 'ERROR';
+        }
+        str += word;
+    }
+    return str;
 };
 
 // Do not edit below this line
