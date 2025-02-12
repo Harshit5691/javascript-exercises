ac2580b23ef0306fbd270bec50305bce75b4911e
Harshit5691 <harshit2.mitmpl2022@learner.manipal.edu>
2025-02-12 20:22:58 +0530

    q5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(l,b) {
+    if (!Number.isInteger(l) || !Number.isInteger(b)) return "ERROR";
+    if (l < 0 || b < 0) return "ERROR";
+    if (l > b) {
+      const temp = l;
+      l = b;
+      b = temp;
+    }
+    let sum =  0;
+    for(let i=l;i<=b;i++)
+        sum += i;
+    return sum;
 };
 
 // Do not edit below this line
